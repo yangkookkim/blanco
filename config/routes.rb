55d42ed465537d5e27806494d98527e71e95f54c
Kim <hiro@lagavulin.local>
2011-12-31 12:24:48 +0900

    before creating profile model

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,13 @@ Blanco::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  resources :employees do
+    member do
+        get 'home'
+    resources :groups do
+        resources :posts do
+        end
+    end
+  end
+  end
 end
