5c030a9a0847e1bee178e5f0004b9f146d2f62f3
Kim <hiro@lagavulin.local>
2012-01-09 23:44:37 +0900

    minor update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def create
   #return redirect_to '/404.html' unless request.xhr?
   @post = Post.new(params[:post])
-  puts "DEBUG"
   if @post.save
     html = render_to_string :partial => "/post/each_post", :collection => [@post]
     render :json => {:success => 1, :html => html, :lastpost_id => @post.id}
