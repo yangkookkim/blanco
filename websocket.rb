b7444beed439161b172ddb3cebd9c629f0083350
Kim <hiro@lagavulin.local>
2012-01-30 02:19:38 +0900

    finish first phase of carrier wave

--- websocket.rb
+++ websocket.rb
@@ -42,46 +42,48 @@ EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 51234) do |ws|
   }
   
   ws.onmessage { |data| 
-    msg = JSON.parse(data) # msg is JSON 
+    msg = JSON.parse(data) # msg is JSON
     channel_id = msg["channel"]["id"]
-    if msg["post"]["img_id"] == 999
-      puts "START CON"
-      # First person to join this channel
-      setup_channel(ws, channel_id, channels) unless channels.key?(channel_id)
-      # People who join the channel already setup
-      join_channel(ws, channel_id, channels) unless channels[channel_id].include?(ws)
-    elsif msg["post"]["img_id"] == 0
-      puts "NEW POST"
-      message = msg["post"]["message"]
-      employee_id  = msg["post"]["employee_id"]
-      group_id = msg["post"]["group_id"]
-      @post = Post.new(:message => message, :employee_id => employee_id, :group_id => group_id)
-      @post.save
-      @employee = @post.employee
-      s = concat_json_object(@post, @employee)
-      ws.send(s)
-      channels[channel_id].each {|con|
-        con.send(msg["post"]["message"].force_encoding("UTF-8")) unless con == ws #to other people
-      }
-    else
-      puts "UPDATE POST"
-      id = msg["post"]["img_id"].to_i
-      message = msg["post"]["message"]
-      employee_id  = msg["post"]["employee_id"]
-      group_id = msg["post"]["group_id"]
-      puts "DEBUG1"
-      @post = Post.find(id)
-      @post.to_json
-      puts "DEBUG2"
-      @post.update_attribute(:message, message)
-      @employee = @post.employee
-      s = concat_json_object(@post, @employee)
-      puts "DEBUG3"
-      ws.send(s)
-      channels[channel_id].each {|con|
-        con.send(msg["post"]["message"].force_encoding("UTF-8")) unless con == ws #to other people
-      }
-    end
+  if msg["ops"] == "setupcon"
+        puts "OPS: START CON"
+        # First person to join this channel
+        setup_channel(ws, channel_id, channels) unless channels.key?(channel_id)
+        # People who join the channel already setup
+        join_channel(ws, channel_id, channels) unless channels[channel_id].include?(ws)
+        puts "DEBUG CHANNELS"
+  elsif msg["ops"] == "sendmsg"
+      if msg["post"]["img_id"] == 0
+        puts "OPS: SENDMSG, NEW POST"
+        message = msg["post"]["message"]
+        employee_id  = msg["post"]["employee_id"]
+        group_id = msg["post"]["group_id"]
+        @post = Post.new(:message => message, :employee_id => employee_id, :group_id => group_id)
+        @post.save
+        @employee = @post.employee
+        s = concat_json_object(@post, @employee)
+        ws.send(s)
+        channels[channel_id].each {|con|
+          con.send(s) unless con == ws #to other people
+        }
+      else
+        puts "OPS: SENDMSG, UPDATE POST"
+        id = msg["post"]["img_id"].to_i
+        message = msg["post"]["message"]
+        employee_id  = msg["post"]["employee_id"]
+        group_id = msg["post"]["group_id"]
+        @post = Post.find(id)
+        @post.to_json
+        @post.update_attribute(:message, message)
+        @employee = @post.employee
+        s = concat_json_object(@post, @employee)
+        ws.send(s)
+        channels[channel_id].each {|con|
+          con.send(s) unless con == ws #to other people
+        }
+      end
+  else
+    puts "OPS: UNKNOWN"
+  end
   }
   
   ws.onclose {
